0a62ee0aa8efb55750c58806eacfee0138407ee2
Marv <marvellousniyi123@gmail.com>
2023-04-28 21:26:41 +0100

    Done

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module defines a function that returns the floor of a float.
+"""
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float.
+
+    Args:
+        n: The float to take the floor of.
+
+    Returns:
+        The floor of n (as an integer).
+    """
+    return math.floor(n)
+
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module defines a function that returns the string representation of a float.
+"""
+
+def to_str(n: float) -> str:
+    """
+    Return the string representation of a float.
+
+    Args:
+        n: The float to convert to a string.
+
+    Returns:
+        The string representation of n.
+    """
+    return str(n)
+
